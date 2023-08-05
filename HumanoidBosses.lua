c0a97851ba4387fa1a3f94041cef8b2e3baafc4d
false-commits <132003518+false-commits@users.noreply.github.com>
2023-08-05 05:52:16 -0700

    Update HumanoidBosses.lua

--- HumanoidBosses.lua
+++ HumanoidBosses.lua
@@ -1,15 +1,56 @@
-- CREDIT RayField 
+--CREDIT RayField 
 -- By HumanoidBosses
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Index = game:GetService("ReplicatedStorage").Badge:FireServer("Double Bladed Scythe", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Cosmic Godsword", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Crimson Energy Bomb", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Corruption Dragon", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Beast Potion", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("DIVINE IRON MAN SUIT", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Dark Energy Bomb", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Death Trident", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Dragon Sword&Shield", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Easter Magical Sword", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Emerald Spellbook of the Gods", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Energy Bomb", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Festive Magical Sword", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Mark XXX SUIT", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Pumpkin Mech", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Star Hammer", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Super Energy Bomb", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Wormhole Gun", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Zombie Mech", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Holiday Hydra", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Crimson Typhoon", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Gaster Blaster", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Halloween Sword 2019", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Halloween Sword 2020", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Nuke Launcher", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Red Laser Scythe", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Dark Crystal Godsword", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Force Gun", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Ghost Mech", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Blue Magical Sword", true)
+		game:GetService("ReplicatedStorage").Badge:FireServer("Soldier Army", true)
 
 
 local Window = Rayfield:CreateWindow({
 	Name = "MEGA Boss Survival",
-	LoadingTitle = "Mega Boss Survival",
+	LoadingTitle = "MEGA Boss Survival",
 	LoadingSubtitle = "by HumanoidBosses",
 	ConfigurationSaving = {
 		Enabled = false,
 		FolderName = nil, 
 		FileName = "Big Hub"	
 },
+})
+
+local MainTab = Window:CreateTab("🏠 Main", nil) 
+local MainSection = MainTab:CreateSection("Main")
+
+local Button = MainTab:CreateButton({
+	Name = "Give Most Items",
+	Callback = function()
+local Index
+	end,
 })
