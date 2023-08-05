1ad99172a185a2273d9d5115d8b4bb669a518942
false-commits <132003518+false-commits@users.noreply.github.com>
2023-08-05 04:47:16 -0700

    Add files via upload

---
+++ HumanoidBosses.lua
@@ -0,0 +1,15 @@
+- CREDIT RayField 
+-- By HumanoidBosses
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+
+local Window = Rayfield:CreateWindow({
+	Name = "MEGA Boss Survival",
+	LoadingTitle = "Mega Boss Survival",
+	LoadingSubtitle = "by HumanoidBosses",
+	ConfigurationSaving = {
+		Enabled = false,
+		FolderName = nil, 
+		FileName = "Big Hub"	
+},
+})
